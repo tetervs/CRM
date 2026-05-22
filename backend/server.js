68125061297a4ee2940864c65309791299bfdfe0
tetervs <adityateterve@gmail.com>
2026-05-22 10:22:15 +0530

    modified:   backend/server.js 	modified:   backend/utils/mailer.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('dns').setDefaultResultOrder('ipv4first') // Render free tier blocks IPv6
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
